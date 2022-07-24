8a377f19e2c06d680b29fb1f9c562025db1f04b9
KimberlyPrieto <k.prieto709@gmail.com>
2022-07-24 14:48:40 -0400

    Saying Hello, World
    changing the intro text in div=className "App" to say hello-world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello World! my name is Kimberly. 
         </p>
         <a
           className="App-link"
